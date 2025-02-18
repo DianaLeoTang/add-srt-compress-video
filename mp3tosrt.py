e57578b4deccb2177850fbd5e88e1508ff9176d4
DianaLeoTang <wangyaotang0228@gmail.com>
2025-02-18 10:59:28 +0800

    feat:Generate an SRT file from an MP3 file.

---
+++ mp3tosrt.py
@@ -0,0 +1,30 @@
+'''
+Author: Diana Tang
+Date: 2025-02-18 10:56:37
+LastEditors: Diana Tang
+Description: some description
+FilePath: /add-srt-compress-video/mp3tosrt.py
+'''
+
+from google.cloud import speech_v1p1beta1 as speech
+import os
+
+# 设置 Google Cloud 的 JSON 凭证
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "path/to/your/credentials.json"
+
+client = speech.SpeechClient()
+
+# 配置音频文件信息
+audio = speech.RecognitionAudio(uri="./1-1chinese.mp3")
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.MP3,
+    language_code="en-US",
+    enable_word_time_offsets=True,
+)
+
+# 开始转录
+response = client.long_running_recognize(config=config, audio=audio)
+
+for result in response.results:
+    for word_info in result.alternatives[0].words:
+        print(f"Word: {word_info.word}, start: {word_info.start_time.total_seconds()}, end: {word_info.end_time.total_seconds()}")
